e1280c0fd7096230bd33ebe6db9b80c80f7f6531
MR-Xyaa <102247436+MR-Xyaa@users.noreply.github.com>
2022-03-24 20:28:26 +0700

    Update please.py

--- module/please.py
+++ module/please.py
@@ -1,53 +1,35 @@
-pkg install cowsay
-pkg install lolcat
-pkg install sl
-pkg install libcaca
-cowsay -f beavis.zen MR-Xyaa | lolcat
-cowsay -f bong MR-Xyaa | lolcat
-cowsay -f bud-MR-Xyaa | lolcat
-cowsay -f bunny MR-Xyaa | lolcat
-cowsay -f cheese MR-Xyaa | lolcat
-cowsay -f cower MR-Xyaa | lolcat
-cowsay -f daemon MR-Xyaa | lolcat
-cowsay -f default MR-Xyaa | lolcat
-cowsay -f dragon-and-cow MR-Xyaa | lolcat
-cowsay -f dragon MR-Xyaa | lolcat
-cowsay -f elephant-in-snake MR-Xyaa | lolcat
-cowsay -f elephant MR-Xyaa | lolcat
-cowsay -f eyes MR-Xyaa | lolcat
-cowsay -f flaming-sheep MR-Xyaa | lolcat
-cowsay -f ghostbusters MR-Xyaa | lolcat
-cowsay -f head-in MR-Xyaa | lolcat
-cowsay -f hellokitty MR-Xyaa | lolcat
-cowsay -f kiss MR-Xyaa | lolcat
-cowsay -f kitty MR-Xyaa | lolcat
-cowsay -f koala MR-Xyaa | lolcat
-cowsay -f kosh MR-Xyaa | lolcat
-cowsay -f luke-koala MR-Xyaa | lolcat
-cowsay -f mech-and-cow MR-Xyaa | lolcat
-cowsay -f meow MR-Xyaa | lolcat
-cowsay -f milk MR-Xyaa | lolcat
-cowsay -f moofasa MR-Xyaa | lolcat
-cowsay -f moose MR-Xyaa | lolcat
-cowsay -f mutilated MR-Xyaa | lolcat
-cowsay -f ren MR-Xyaa | lolcat
-cowsay -f satanic MR-Xyaa | lolcat
-cowsay -f sheep MR-Xyaa | lolcat
-cowsay -f skeleton MR-Xyaa | lolcat
-cowsay -f small MR-Xyaa | lolcat
-cowsay -f sodomized MR-Xyaa | lolcat
-cowsay -f stegosaurus MR-Xyaa | lolcat
-cowsay -f stimpy MR-Xyaa | lolcat
-cowsay -f supermilker MR-Xyaa | lolcat
-cowsay -f surgery MR-Xyaa | lolcat
-cowsay -f telebears MR-Xyaa | lolcat
-cowsay -f turkey MR-Xyaa | lolcat
-cowsay -f turtle MR-Xyaa | lolcat
-cowsay -f tux MR-Xyaa | lolcat
-cowsay -f udder MR-Xyaa | lolcat
-cowsay -f vader-koala MR-Xyaa | lolcat
-cowsay -f vader MR-Xyaa | lolcat
-cowsay -f www MR-Xyaa | lolcat
-clear
-sl
-cacafire
+import os, sys
+
+print ("\033[1;32mMasukan UserName&Password:)")
+print ("\033[1;31;1mKalo Gak Tau Pm Mr.BAJINGAN 083108271123")
+username = 'Xyaa'      
+password = 'Gans'
+
+def restart():
+	ngulang = sys.executable
+	os.execl(ngulang, ngulang, *sys.argv)
+
+def main():
+	uname = raw_input("username : ")
+	if uname == username:
+		pwd = raw_input("password : ")
+
+		if pwd == password:
+			print "\n\033[1;34mHello Welcome To Tools B4J1N64N", 
+			sys.exit()
+
+		else:
+			print "\n\033[1;36mSorry Invalid Password !!!\033[00m"
+			print "Back Login\n"
+			restart()
+
+	else:
+		print "\n\033[1;36mSorry Invalid Username !!!\033[00m"
+		print "Back Login\n"
+		restart()
+
+try:
+	main()
+except KeyboardInterrupt:
+	os.system('clear')
+	restart()
